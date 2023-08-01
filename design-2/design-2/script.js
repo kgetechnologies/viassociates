487457b690c2a820f4f79525f48f4983e3cb9133
sknaikv <94632098+sknaikv@users.noreply.github.com>
2023-08-01 23:53:36 +0530

    commit-3
    added policies section and animations

--- design-2/design-2/script.js
+++ design-2/design-2/script.js
@@ -47,3 +47,22 @@ function nav(){
 nav();
 herotext();
 heroimg();
+
+
+window.addEventListener('scroll',reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i=0;i<reveals.length;i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop<windowheight-revealpoint){
+            reveals[i].classList.add('active');
+        }else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
