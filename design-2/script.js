3a1a01e89d6ba799a3a24fe5124132cee91c9100
sknaikv <94632098+sknaikv@users.noreply.github.com>
2023-08-03 20:28:29 +0530

    minor changes -mobile view

--- design-2/script.js
+++ design-2/script.js
@@ -173,4 +173,18 @@ function revealclient() {
 
 
 
+const hamburger = document.querySelector('.hamburger');
+const navmenu = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle("active");
+    navmenu.classList.toggle("active");
+})
+
+document.querySelectorAll('.nav-links').forEach(n =>{
+    n.addEventListener('click',()=>{
+        hamburger.classList.remove("active");
+        navmenu.classList.remove("active");
+    })
+})
 
